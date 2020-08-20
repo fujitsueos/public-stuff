b539de80789be3b1084594cf30917127079ca549
masoud <39732800+gorjizadehm@users.noreply.github.com>
2020-08-20 14:03:08 +0200

    Update master.ps1

--- salt/windows/master.ps1
+++ salt/windows/master.ps1
@@ -2,8 +2,6 @@ param(
     [string]$vmname,
     [string]$saltmaster,
     [string]$minionversion,
-    [string]$avversion,
-    [string]$postprovision,
     [string]$installminion,
     [string]$username,
     [string]$password,
@@ -16,8 +14,4 @@ if($installminion -eq $True) {
     .\bootstrap-salt.ps1 -minion $minionid -master $saltmaster -version $minionversion
 
     .\saltclient.ps1 -saltmaster $saltmaster -username $username -password $password -minionid $minionid -appnames $appnames
- }
-
- if($postprovision -eq $True) {
-    .\windows-arm-script.ps1 -version $avversion
  }
