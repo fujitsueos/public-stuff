5822999e1c839962f63938ca54f0f3b5ac22e334
masoud <39732800+gorjizadehm@users.noreply.github.com>
2021-02-09 13:42:27 +0100

    Update master.sh

--- salt/linux/master.sh
+++ salt/linux/master.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 INSTALL_MINION=$(echo $1 | tr '[:upper:]' '[:lower:]')
-MASTER_IP=$2
-USERNAME=$3
-PASSWORD=$4
-MINION_ID=$5
-MINION_VERSION=$6
-APPNAMES=$7
+MASTER_HOSTNAME=$2
+MASTER_URL=$3
+USERNAME=$4
+PASSWORD=$5
+MINION_ID=$6
+MINION_VERSION=$7
+APPNAMES=$8
 
 TOKEN=$(curl  --insecure --request POST \
-  --url "https://${MASTER_IP}/login" \
+  --url "${MASTER_URL}/login" \
   --header 'content-type: application/json' \
   --data "{\"username\": \"${USERNAME}\", \"password\": \"${PASSWORD}\", \"eauth\": \"pam\"}" | \
     python2 -c "import sys, json; print json.load(sys.stdin)['return'][0]['token']")
@@ -17,7 +18,7 @@ TOKEN=$(curl  --insecure --request POST \
 
 install_minion()
 {
-	sudo /bin/bash bootstrap-salt.sh -A $MASTER_IP -i $MINION_ID git "v${MINION_VERSION}"
+	sudo /bin/bash bootstrap-salt.sh -A $MASTER_HOSTNAME -i $MINION_ID git "v${MINION_VERSION}"
 }
 
 autosign_minion()
@@ -25,7 +26,7 @@ autosign_minion()
 
 
 	curl --insecure --request POST \
-	  --url "https://${MASTER_IP}/" \
+	  --url "${MASTER_URL}/" \
 	  --header 'content-type: application/json' \
 	  --header "x-auth-token: ${TOKEN}" \
 	  --data "{\"client\": \"local\", \"tgt\": \"saltmaster\", \"fun\": \"cmd.run\", \"arg\":\"touch /etc/salt/pki/master/minions_autosign/${MINION_ID}\"}"
@@ -38,7 +39,7 @@ install_applications()
 	    APPNAME=$(echo $i | base64 --decode)
 
 	    JID=$(curl  --insecure --request POST \
-		  --url "https://${MASTER_IP}/minions" \
+		  --url "${MASTER_URL}/minions" \
 		  --header 'content-type: application/json' \
 		  --header "x-auth-token: ${TOKEN}" \
 		  --data "{\"client\": \"local\", \"tgt\": \"${MINION_ID}\", \"fun\": \"state.sls\", \"arg\":\"${APPNAME}\"}" | \
