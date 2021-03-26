e9933b3c59eb93904f88ba3eee19d396afa6d92b
masoud <39732800+gorjizadehm@users.noreply.github.com>
2021-03-26 10:17:39 +0100

    Update master.ps1

--- salt/windows/master.ps1
+++ salt/windows/master.ps1
@@ -9,7 +9,13 @@ param(
 
 )
 $saltRepo = "https://archive.repo.saltproject.io/windows"
+
 if ($installminion -eq $True) {
+  $year = $minionversion.Substring(0, 4)
+  If (($minionversion.ToLower() -eq 'latest') -or [int]$year -gt 2019) {
+      $saltRepo = "https://repo.saltproject.io/windows"
+  }
+
   .\bootstrap-salt.ps1 -repourl $saltRepo -minion $minionid -master $salthostname -version $minionversion
 
   .\saltclient.ps1 -saltmaster $salturl -UserName $username -password $password -minionid $minionid
